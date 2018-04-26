4591a61766eef0cc0fc5c407788a6676d875ac57
Ryan Kojan <rkojan@gmail.com>
2018-04-26 11:10:04 -0700

    Created blank django project

---
+++ talent_roster/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ryankojan/Desktop/spring2018/talent-roster/talent_roster/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
