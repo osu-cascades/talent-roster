ecfa9c124e1ebeeb79ae56e88b10ef13af22384a
Yong Bakos <ybakos@humanoriented.com>
2018-04-29 20:36:41 -0700

    Reorganize static assets, add whitenoise package. Heroku requires the whitenoise package and will compile all static assets in the staticfiles directory. Move static prototype files to profiles/static.

--- talent_roster/settings.py
+++ talent_roster/settings.py
@@ -69,6 +69,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'profiles/static'),
+)
 
 django_heroku.settings(locals())
