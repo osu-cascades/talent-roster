79f80a4407022edd55334187fbdaff55d990551f
Yong Bakos <ybakos@humanoriented.com>
2018-05-01 09:22:12 -0600

    Profile: Rename to singular, adjust field names and length. @rkojan Always use singular class names.

--- profiles/models.py
+++ profiles/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 
-class Profiles(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    photo = models.CharField(max_length=100)
-    resume = models.CharField(max_length=100)
-    skills = models.CharField(max_length=250)
-    bio = models.CharField(max_length=250)
-    git_hub = models.CharField(max_length=50)
-    website = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
+class Profile(models.Model):
+    first_name = models.CharField(max_length=40, blank=False)
+    last_name = models.CharField(max_length=40, blank=False)
+    email = models.CharField(max_length=50, blank=False)
+    github_username = models.CharField(max_length=50, blank=False)
+    bio = models.CharField(max_length=500, blank=False)
     graduation_date = models.DateField(blank=False)
-
-    
+    photo = models.CharField(max_length=250)
+    resume = models.CharField(max_length=250)
+    skills = models.CharField(max_length=250)
+    website_url = models.CharField(max_length=50)
