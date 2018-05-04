6cf9501a649086185bd7a19dbf01c85adb0dec92
Yong Bakos <ybakos@humanoriented.com>
2018-05-04 19:12:36 -0400

    Add Profile data loading to list view.

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Profile
 
 
 def index(request):
     return render(request, 'index.html')
 
 def list(request):
-    return HttpResponse("List of profiles")
+    profiles = Profile.objects.order_by('last_name')
+    output = ', '.join([p.full_name for p in profiles])
+    return HttpResponse("List of profiles\n" + output)
 
 def detail(request, profile_id):
     return HttpResponse("Display profile %s" % profile_id)
