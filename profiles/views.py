e8b142cf6818011d087446be2d0cce46708f40a4
Yong Bakos <ybakos@humanoriented.com>
2018-05-04 18:59:08 -0400

    Stub profile list view.

--- profiles/views.py
+++ profiles/views.py
@@ -5,5 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
+def list(request):
+    return HttpResponse("List of profiles")
+
 def detail(request, profile_id):
     return HttpResponse("Display profile %s" % profile_id)
