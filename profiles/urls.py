febe94ccc0a8beff25373e8c3b4eb1423093dea1
Yong Bakos <ybakos@humanoriented.com>
2018-05-04 18:55:09 -0400

    Stub profile detail view.

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:profile_id>/', views.detail, name='detail'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def detail(request, profile_id):
+    return HttpResponse("Display profile %s" % profile_id)
