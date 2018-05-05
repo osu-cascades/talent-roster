2e3ec6501da57e8feb15c8b1b10375e4aa355f4a
Ryan Kojan <rkojan@gmail.com>
2018-05-04 22:20:07 -0700

    updated path for templates

--- profiles/views.py
+++ profiles/views.py
@@ -3,26 +3,26 @@ from django.http import HttpResponse
 from .models import Profile
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'profiles/index.html')
 
 def profile(request):
-    return render(request, 'profile.html', output)
+    return render(request, 'profiles/profile.html', output)
 
 def sign_up(request):
-    return render(request, 'signup.html')
+    return render(request, 'profiles/signup.html')
 
 def log_in(request):
-    return render(request, 'login.html')
+    return render(request, 'profiles/login.html')
 
 def account(request):
-    return render(request, 'account.html')
+    return render(request, 'profiles/account.html')
 
 def business(request):
-    return render(request, 'business.html')
+    return render(request, 'profiles/business.html')
 
 def profiles(request):
 
     all_profiles = Profile.objects.all()
     context = {'first_name': all_profiles}
 
-    return render(request, 'profiles.html',context)
+    return render(request, 'profiles/profiles.html',context)
