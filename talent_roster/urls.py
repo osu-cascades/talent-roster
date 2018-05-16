743cf24bd77d28b4d8f641bb80908b41f458ab0d
Yong Bakos <ybakos@humanoriented.com>
2018-05-15 21:16:08 -0700

    Set root url.

--- talent_roster/urls.py
+++ talent_roster/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from profiles import views
 
 urlpatterns = [
+    path('', views.index),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
