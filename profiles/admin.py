9e786e90f859ae02c9808ba7fe4ce4d054fd5693
Yong Bakos <ybakos@humanoriented.com>
2018-05-29 01:50:33 -0700

    Admin: Improve profile form.

--- profiles/admin.py
+++ profiles/admin.py
@@ -4,13 +4,13 @@ from .models import Profile
 
 
 class ProfileForm(forms.ModelForm):
-    bio = forms.CharField(widget = forms.Textarea(attrs={'rows': 5, 'cols': 100}))
+    bio = forms.CharField(widget = forms.Textarea(attrs={'rows': 5, 'cols': 100}), help_text='500 character maximum')
 
 
 class ProfileAdmin(admin.ModelAdmin):
     form = ProfileForm
     fieldsets = [
-        (None, {'fields':[('first_name','last_name'), ('email_address', 'graduation_date'), 'bio', 'photo', 'resume', 'github_username','website_url']}),
+        (None, {'fields':[('first_name','last_name'), ('email_address', 'graduation_date'), 'bio', ('photo', 'resume'), 'github_username','website_url']}),
     ]
     list_display = ('last_name', 'first_name', 'email_address', 'graduation_date')
     list_filter =['graduation_date']
