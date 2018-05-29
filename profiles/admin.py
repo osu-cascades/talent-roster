a6ff56ffafb4bea604b4ba011a78179687e3a551
Yong Bakos <ybakos@humanoriented.com>
2018-05-29 01:20:27 -0700

    ProfileAdmin: Consolidate fields, rearrange list columns, use textarea for bio.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
+from django import forms
 from .models import Profile
 
+
+class ProfileForm(forms.ModelForm):
+    bio = forms.CharField(widget = forms.Textarea(attrs={'rows': 5, 'cols': 100}))
+
+
 class ProfileAdmin(admin.ModelAdmin):
+    form = ProfileForm
     fieldsets = [
-        ('Personal Informaion', {'fields':['first_name','last_name', 'email','bio']}),
-        ('Profile Informaion', {'fields':['graduation_date','skills','github_username','photo','resume','website_url']}),
+        (None, {'fields':[('first_name','last_name'), ('email', 'graduation_date'), 'bio', 'photo', 'resume', 'skills', 'github_username','website_url']}),
     ]
-    list_display = ('email', 'first_name', 'last_name','graduation_date')
+    list_display = ('last_name', 'first_name', 'email', 'graduation_date')
     list_filter =['graduation_date']
 
+
 admin.site.register(Profile, ProfileAdmin)
-
-
