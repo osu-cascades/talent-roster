7110adbfef5964d709de9aa6bdd519b9d2b61654
Yong Bakos <ybakos@humanoriented.com>
2018-05-29 10:13:18 -0700

    Admin: Customize header text and title text.

--- talent_roster/urls.py
+++ talent_roster/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from profiles import views
 
+admin.site.site_header = 'Talent Roster Administration'
+admin.site.site_title = 'Talent Roster Administration'
+
 urlpatterns = [
     path('', views.index),
     path('profiles/', include('profiles.urls')),
