0fb1b9e9737383dc0b6993ac2bfdfa2dc0c4d3d3
Yong Bakos <ybakos@humanoriented.com>
2018-05-29 09:51:36 -0700

    Admin: Use TabularInline for profile skills.

--- profiles/admin.py
+++ profiles/admin.py
@@ -4,10 +4,10 @@ from .models import Profile, Skill
 
 
 class ProfileForm(forms.ModelForm):
-    bio = forms.CharField(widget = forms.Textarea(attrs={'rows': 5, 'cols': 100}), help_text='500 character maximum')
+    bio = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 100}), help_text='500 character maximum')
 
 
-class SkillInline(admin.StackedInline):
+class SkillInline(admin.TabularInline):
     model = Skill
     extra = 5
     max_num = 5
@@ -17,7 +17,7 @@ class ProfileAdmin(admin.ModelAdmin):
     form = ProfileForm
     fields = [('first_name','last_name'), ('email_address', 'graduation_date'), 'bio', ('photo', 'resume'), 'github_username','website_url']
     list_display = ('last_name', 'first_name', 'email_address', 'graduation_date')
-    list_filter =['graduation_date']
+    list_filter = ['graduation_date']
     inlines = [SkillInline]
 
 
