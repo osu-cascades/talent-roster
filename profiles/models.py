2415817eda360fa45995524c55606ccf2ce94bd0
Ryan Kojan <rkojan@gmail.com>
2018-05-29 21:00:22 -0700

    Index: Placed HTML mockup for featured profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 def profile_media_path(instance, filename):
     return 'profiles/owner_{0}/{1}'.format('TODO', filename)
 
@@ -11,7 +12,7 @@ class Profile(models.Model):
     email_address = models.EmailField(blank=False)
     graduation_date = models.DateField(blank=False)
     bio = models.CharField(max_length=500, blank=False)
-    photo = models.ImageField(upload_to=profile_media_path)
+    #photo = models.ImageField(upload_to=profile_media_path)
     resume = models.FileField(upload_to=profile_media_path)
     github_username = models.CharField(max_length=50, blank=False)
     website_url = models.URLField()
